ec9da0de48853c2fb03923eeea97cbc1376f7cee
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-03-29 19:12:06 +0530

    Setup 1a1a18

---
+++ polybar/pkg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pac=$(checkupdates | wc -l)
+aur=$(cower -u | wc -l)
+
+check=$((pac + aur))
+if [[ "$check" != "0" ]]
+then
+    echo "$pac %{F#CACAC8}%{F-} $aur"
+else
+	echo "%{F#CACAC8}%{F-}"
+fi
