39a3fd2f13088b77c60682151f860f7e8ae27826
Soorya Narayan <soorya.narayan.jm@gmail.com>
2019-08-21 21:15:54 +0530

    21/08/2019

--- polybar/mpris.sh
+++ polybar/mpris.sh
@@ -9,18 +9,11 @@ if [[ $? -eq 0 ]]; then
     metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
 fi
 
-mpc_status=$(mpc -p 7000 | sed -n 2p | cut -d ' ' -f1)
-mpc_metadata="$(mpc -p 7000 -f ' %artist% - %title%' current)"
-
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
     echo "$icon $metadata"       # Orange when playing
-elif [[ $mpc_status = "[playing]" ]]; then
-	echo "$icon $mpc_metadata"       # Orange when playing
 elif [[ $player_status = "Paused" ]]; then
     echo "%{F#65737E}$icon $metadata"       # Greyed out info when paused
-elif [[ $mpc_status = "[paused]" ]]; then
-	echo "%{F#65737E}$icon $mpc_metadata"       # Greyed out info when paused
 else
     echo ""                 # Greyed out icon when stopped
 fi
---
+++ qutebrowser/config.py
@@ -0,0 +1,3 @@
+config.source('nord-qutebrowser.py')
+# c.content.user_stylesheets = '~/dotfiles/qutebrowser/nord.css'
+c.tabs.position = "bottom"
